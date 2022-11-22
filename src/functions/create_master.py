58b9e0865dd94c456b0f74763ed0ae4056ed6921
Niels Wouda <nielswouda@gmail.com>
2022-11-22 16:56:53 +0100

    Implement create_master

--- src/functions/create_master.py
+++ src/functions/create_master.py
@@ -1,3 +1,5 @@
+from gurobipy import Model
+
 from src.classes import MasterProblem, ProblemData
 
 
@@ -7,4 +9,28 @@ def create_master(
     """
     TODO
     """
-    pass
+    m = Model()
+
+    m.addMVar((data.num_edges,), obj=data.cost, vtype=data.vtype, name="x")
+
+    z = m.addMVar((data.num_scenarios,), vtype="B", name="z")
+    m.addConstr(z.sum() <= alpha * data.num_scenarios, name="scenarios")
+
+    m.update()
+
+    A = m.getA()
+    constrs = m.getConstrs()
+    dec_vars = m.getVars()
+
+    obj = [var.obj for var in dec_vars]
+    lb = [var.lb for var in dec_vars]
+    ub = [var.ub for var in dec_vars]
+    vtype = [var.vtype for var in dec_vars]
+    vname = [var.varName for var in dec_vars]
+    cname = [constr.constrName for constr in constrs]
+    b = [constr.rhs for constr in constrs]
+    senses = [constr.sense for constr in constrs]
+
+    return MasterProblem(
+        obj, A, b, senses, vtype, lb, ub, vname, cname, data.num_scenarios
+    )
